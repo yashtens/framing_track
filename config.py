cdd35a1bfe7c97bce2e10d172ae3e4373506b719
Yash marathe <maratheyash58@gmail.com>
2026-02-26 19:19:44 +0530

    new_file

--- config.py
+++ config.py
@@ -8,15 +8,28 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', '579b464db66ec23bdd000001cdd3946e44ce4aad7209ff7b23ac571b')
 
     # MySQL Database Configuration
-    DB_HOST = os.environ.get('DB_HOST', 'localhost')
+    # DB_HOST = os.environ.get('DB_HOST', 'localhost')
+    # DB_USER = os.environ.get('DB_USER', 'root')
+    # DB_PASSWORD = os.environ.get('DB_PASSWORD', 'yash1947#')
+    # DB_NAME = os.environ.get('DB_NAME', 'krishitrack_db')
+    # DB_PORT = os.environ.get('DB_PORT', '3306')
+
+    # SQLALCHEMY_DATABASE_URI = (
+    #     f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    # )
+
+    # MySQL Database Configuration
+    DB_HOST = os.environ.get('DB_HOST', 'mysql.railway.internal')
     DB_USER = os.environ.get('DB_USER', 'root')
-    DB_PASSWORD = os.environ.get('DB_PASSWORD', 'yash1947#')
-    DB_NAME = os.environ.get('DB_NAME', 'krishitrack_db')
+    DB_PASSWORD = os.environ.get('DB_PASSWORD', 'sHHJinOioikhTDfisRAyYBSaAoLHnGFU')
+    DB_NAME = os.environ.get('DB_NAME', 'railway')
     DB_PORT = os.environ.get('DB_PORT', '3306')
 
     SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+        "mysql: // root: sHHJinOioikhTDfisRAyYBSaAoLHnGFU @ mysql.railway.internal:3306 / railway"
     )
+
+    # mysql: // root: sHHJinOioikhTDfisRAyYBSaAoLHnGFU @ mysql.railway.internal:3306 / railway
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), 'uploads')
