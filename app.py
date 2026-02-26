01bed11b19e38762903ea9dd1018de803a9c4779
Yash marathe <maratheyash58@gmail.com>
2026-02-26 09:56:12 +0530

    new_port

--- app.py
+++ app.py
@@ -1211,4 +1211,5 @@ def seed_sample_data(Crop, Expense, Labour, Harvest):
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host="0.0.0.0", port=port)
